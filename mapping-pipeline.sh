07d77a92652650d5d6147721fb227357fd13dc1b
adina <howead@msu.edu>
2017-02-17 12:10:09 -0500

    update

--- mapping-pipeline.sh
+++ mapping-pipeline.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -login
-#PBS -l walltime=150:00:00,nodes=01:ppn=8,mem=10gb
+#PBS -l walltime=50:00:00,nodes=01:ppn=8,mem=80gb
 #PBS -q main
 #PBS -M adina.chuang@gmail.com
 #PBS -m abe
 #PBS -A ged
 
-MY_PATH=/mnt/home/howead/metag-pipeline
+MY_PATH=/mnt/home/howead/metags-darte-round2-env
 MAPPER_PATH=/mnt/home/howead/software/bowtie2-2.2.9
 SAMTOOLS_PATH=/mnt/home/howead/software/samtools-1.3.1
 THREADS=8
 PAR_PATH=/mnt/home/howead/software/parallel-20120122/src
-CONTIGS_FILE=/mnt/home/howead/metag-pipeline/assembly/contigs.test.fa
+CONTIGS_FILE=/mnt/home/howead/metags-darte-round2-env/manure-assembly-all/manure.contigs.fa
 CONTIGS_FILE_B=`basename $CONTIGS_FILE`
 
 cd $MY_PATH
@@ -27,3 +27,14 @@ for x in mapping-data/*bam; do echo "$SAMTOOLS_PATH/samtools sort $x -o $x.sorte
 cat samtools.sort.sh | $PAR_PATH/parallel
 for x in mapping-data/*sorted; do echo "$SAMTOOLS_PATH/samtools index $x"; done > samtools-index.sh
 cat samtools-index.sh | $PAR_PATH/parallel
+for x in mapping-data/*sorted; do echo "$SAMTOOLS_PATH/samtools idxstats $x > $x.idxstats"; done > samtools-idx.sh
+cat samtools-idx.sh | $PAR_PATH/parallel
+
+module load bedtools
+
+for x in mapping-data/*sorted; do echo "bamToBed -i $x > $x.bed"; done > bamtobed.sh
+cat bamtobed.sh | $PAR_PATH/parallel
+for x in mapping-data/*bed; do echo "coverageBed -a $x -b $CONTIGS_FILE.bed -d > $x.bed2"; done > coveragebed.sh
+cat coveragebed.sh | $PAR_PATH/parallel
+for x in mapping-data/*bed2; do echo "python bedcoverage-to-coverage.py $x > $x.counts"; done > bedcoveragefinal.sh
+cat bedcoveragefinal.sh | $PAR_PATH/parallel
